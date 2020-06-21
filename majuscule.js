4334ce442c9fe5f99b4b2cf6185dcd2a5195cea1
jervalles <jer.valles@outlook.com>
2020-06-21 17:20:02 +0200

    modification if else to ternary operator

--- majuscule.js
+++ majuscule.js
@@ -4,11 +4,7 @@ function majuscule(arg) {
     let text = arg.split('')
 
     for (let i = 0; i < text.length; i++) {
-        if (i % 2 == 0) {
-            text[i] = text[i].toLowerCase()
-        } else {
-            text[i] = text[i].toUpperCase()
-        }
+        i % 2 == 0 ? text[i] = text[i].toLowerCase() : text[i] = text[i].toUpperCase()
     }
     console.log(text.join(''))
 }
