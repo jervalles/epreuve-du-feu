8c614be158255cfef08581c7fcbd2bef34cf923e
jervalles <jer.valles@outlook.com>
2020-06-21 18:02:56 +0200

    taking space condition

--- majuscule.js
+++ majuscule.js
@@ -3,8 +3,18 @@ let arg = process.argv[2]
 function majuscule(arg) {
     let text = arg.split('')
 
-    for (let i = 0; i < text.length; i++) {
-        i % 2 == 0 ? text[i] = text[i].toLowerCase() : text[i] = text[i].toUpperCase()
+    for (let i = 0, k = 0; i < text.length; i++) {
+        if (text[i] == ' ') {
+            continue
+        } else {
+            k++
+        }
+        if (k % 2 == 0) {
+            text[i] = text[i].toUpperCase()
+
+        } else {
+            text[i] = text[i].toLowerCase()
+        }
     }
     console.log(text.join(''))
 }
